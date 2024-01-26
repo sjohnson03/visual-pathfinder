9bf23efee70140f603a9f06dbf29ec356458dc4b
Shannon Johnson <shannon.johnson0301@gmail.com>
2024-01-26 23:36:43 +1100

    Refactored drawGrid function to use BLOCKSIZE constant

--- src/main.py
+++ src/main.py
@@ -1,27 +1,27 @@
 # Example file showing a basic pygame "game loop"
+from time import sleep
 import pygame
 from entry import createEntry
 
 BLACK = (0,0,0)
 WIDTH = 800
 HEIGHT = 800
+BLOCKSIZE = 20
 
 def drawGrid():
-    blockSize = 20
-    for x in range(0, WIDTH, blockSize):
-        for y in range(0, HEIGHT, blockSize):
-            rect = pygame.Rect(x, y, blockSize, blockSize)
+    for x in range(0, WIDTH, BLOCKSIZE):
+        for y in range(0, HEIGHT, BLOCKSIZE):
+            rect = pygame.Rect(x, y, BLOCKSIZE, BLOCKSIZE)
             pygame.draw.rect(screen, "white", rect, 1)
 
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
-screen.fill(BLACK)
 drawGrid()
 
 running = True
-
+coordinates = createEntry(WIDTH / BLOCKSIZE, HEIGHT / BLOCKSIZE)
 
 
 while running:
